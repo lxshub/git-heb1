1cf951b6793dbd91fd8f5870cd4f97a20e4fce65
Lee <320011927@qq.com>
2021-11-16 21:25:24 +0800

    idea commit1

---
+++ src/main/java/com/lee/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lee.git;
+
+/**
+ * @author lixianshuai
+ * @date 2021/11/16 21:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
