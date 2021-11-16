04b9812a6e7df988e9de0d7b1f7839803236f66e
Lee <320011927@qq.com>
2021-11-16 21:26:41 +0800

    idea commit2

--- src/main/java/com/lee/git/GitTest.java
+++ src/main/java/com/lee/git/GitTest.java
@@ -7,5 +7,6 @@ package com.lee.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git1!");
     }
 }
